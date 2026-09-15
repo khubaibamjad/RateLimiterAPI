b7b8744a4e950b8725626339369e0d1d87124e88
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-15 03:34:56 -0700

    Initial commit

---
+++ src/test/java/com/example/LaterLimiter/LaterLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaterLimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaterLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
